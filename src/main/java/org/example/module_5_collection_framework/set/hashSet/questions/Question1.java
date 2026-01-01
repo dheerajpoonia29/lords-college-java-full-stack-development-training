eb5098a4257328fdeb2b67250bc55e54ee04d229
Dheeraj Poonia <dheerajbishnoi123@gmail.com>
2026-01-01 19:55:08 +0530

    learning hashset

---
+++ src/main/java/org/example/module_5_collection_framework/set/hashSet/questions/Question1.java
@@ -0,0 +1,14 @@
+package org.example.module_5_collection_framework.set.hashSet.questions;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Question1 {
+    public static void main(String[] args) {
+        ArrayList<Integer> input = new ArrayList<>(Arrays.asList(1, 2, 3, 2, 4, 1, 5));
+
+        // logic to print duplicate elements
+
+
+    }
+}
