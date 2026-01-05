2c567b3b05542ea7081681aaaad25c7bf4c0c0b3
Dheeraj Poonia <dheerajbishnoi123@gmail.com>
2026-01-05 19:59:24 +0530

    generic function

---
+++ src/main/java/org/example/module_5_collections_map_generic/generic_programming/Demo.java
@@ -0,0 +1,12 @@
+package org.example.module_5_collections_map_generic.generic_programming;
+
+import java.util.ArrayList;
+
+public class Demo {
+    public static void main(String[] args) {
+        ArrayList<Integer> arr1 = new ArrayList<>();
+        // arr1 is type of Integer
+        ArrayList<String> arr2 = new ArrayList<>();
+        // arr2 is type of String
+    }
+}
---
+++ src/main/java/org/example/module_5_collections_map_generic/generic_programming/generic_method/MyClass.java
@@ -0,0 +1,31 @@
+package org.example.module_5_collections_map_generic.generic_programming.generic_method;
+
+public class MyClass {
+    public static void main(String[] args) {
+        MyClass myClass = new MyClass();
+        myClass.method1("Hello, World!");
+        myClass.method1(42);
+        myClass.method1(3.14);
+
+        String result1 = myClass.method2("Generic Method");
+        Integer result2 = myClass.method2(100);
+        System.out.println("Result1: " + result1);
+        System.out.println("Result2: " + result2);
+
+        System.out.println(myClass.method3("First Parameter", 200));
+        System.out.println(myClass.method3(2.14, "Second Parameter"));
+    }
+
+    // Generic method that accepts a parameter of any type
+    public <T> void method1(T parameter) {
+        System.out.println("Parameter value: " + parameter);
+    }
+
+    public <T> T method2(T parameter) {
+        return parameter;
+    }
+
+    public <T, Z> T method3(T param1, Z param2) {
+        return param1;
+    }
+}
