6c4cc0dfd47c0cb4b7f026d893ace63477073c6f
Dheeraj Poonia <dheerajbishnoi123@gmail.com>
2026-01-07 20:51:00 +0530

    text file handling

---
+++ src/main/java/org/example/module_6_file_handling/character_stream/csv_file/Person.java
@@ -0,0 +1,13 @@
+package org.example.module_6_file_handling.character_stream.csv_file;
+
+public class Person {
+    String name;
+    Integer age;
+    String city;
+
+    Person(String name, Integer age, String city) {
+        this.name = name;
+        this.age = age;
+        this.city = city;
+    }
+}
