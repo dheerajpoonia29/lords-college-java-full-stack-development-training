ab1cf8ead95836327db4d053ba8c39fa587c2bfd
Dheeraj Poonia <dheerajbishnoi123@gmail.com>
2026-01-10 20:53:58 +0530

    read json file and get into java object

--- src/main/java/org/example/module_6_file_handling/character_stream/json_file/Person.java
+++ src/main/java/org/example/module_6_file_handling/character_stream/json_file/Person.java
@@ -4,4 +4,29 @@ public class Person {
     String personName;
 
     Address address;
+
+    public String getPersonName() {
+        return personName;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setPersonName(String personName) {
+        this.personName = personName;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "personName='" + personName + '\'' +
+                ", address street=" + address.street +
+                " city =" + address.city +
+                '}';
+    }
 }
--- src/main/java/org/example/module_6_file_handling/character_stream/json_file/ReadJsonFile.java
+++ src/main/java/org/example/module_6_file_handling/character_stream/json_file/ReadJsonFile.java
@@ -1,5 +1,7 @@
 package org.example.module_6_file_handling.character_stream.json_file;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
@@ -10,11 +12,14 @@ public class ReadJsonFile {
         FileReader obj = new FileReader("src/main/java/org/example/module_6_file_handling/character_stream/json_file/address.json");
         BufferedReader obj2 = new BufferedReader(obj);
 
-        Person personObj = new Person();
-
+        // we can not read json file line by line because of it's different data structure
 //        String line;
 //        while ((line = obj2.readLine()) != null) {
 //            System.out.println(line);
 //        }
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        Person personObj = objectMapper.readValue(obj2, Person.class);
+        System.out.println(personObj.toString());
     }
 }
