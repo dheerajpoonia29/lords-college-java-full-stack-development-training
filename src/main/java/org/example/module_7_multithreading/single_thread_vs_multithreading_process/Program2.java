9bc73a37b1fef5f986abb480796ce2ce6bee1413
Dheeraj Poonia <dheerajbishnoi123@gmail.com>
2026-01-16 20:34:22 +0530

    start multithreaded in java

--- src/main/java/org/example/module_7_multithreading/single_thread_vs_multithreading_process/Program2.java
+++ src/main/java/org/example/module_7_multithreading/single_thread_vs_multithreading_process/Program2.java
@@ -4,10 +4,11 @@ public class Program2 {
     public static void main(String[] args) {
 
         // Program2 is multi-threaded program
+        // jvm will also hold 2 mb ram for program1
 
-        Task1Thread t1Thread = new Task1Thread();
-        Task2Thread t2Thread = new Task2Thread();
-        Task3Thread t3Thread = new Task3Thread();
+        Task1Thread t1Thread = new Task1Thread(); // 1 kb
+        Task2Thread t2Thread = new Task2Thread(); // 1 kb
+        Task3Thread t3Thread = new Task3Thread(); // 1 kb
 
         t1Thread.start();
         t2Thread.start();
@@ -16,12 +17,14 @@ public class Program2 {
 }
 
 class Task1Thread extends Thread {
+    int a = 10;
     public void run() {
         System.out.println("task1 is running");
     }
 }
 
 class Task2Thread extends Thread {
+    int a = 10;
     public void run() {
         System.out.println("task2 is running");
     }
